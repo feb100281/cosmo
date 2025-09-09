fda17b9c26329585c96a171c559acbe7cc1aada2
feb100281 <feb100281@gmail.com>
2025-09-09 23:31:55 +0300

    try

--- utils/updater.py
+++ utils/updater.py
@@ -116,6 +116,7 @@ def set_data(d:dict):
     df = pd.read_sql("SELECT * FROM new_sales", con=engine)
     df['client_order_date'] = pd.to_datetime(df['client_order_date'])
     df = df.sort_values(by='client_order_date')
+    df['client_order_date'] = df['client_order_date'].fillna('date')
     
     df = df.pivot_table(
         index = 'fullname',
